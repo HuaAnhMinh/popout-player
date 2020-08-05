0abce638793297a346e336610f42c8ba30617277
earlwlkr <mike358015@gmail.com>
2020-08-05 15:21:54 +0700

    Bootstrap

---
+++ README.md
@@ -0,0 +1,6 @@
+Popout Video Player
+
+## Yêu cầu
+
+Từ 1 URL nhập ở inputbox, bung ra 1 player khi nhấn button *Play*.
+Có thể resize player này như 1 window bình thường và kéo đến vị trí mong muốn.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input placeholder="Youtube URL" />
+      <button>Play</button>
+    </div>
+  );
+}
+
+export default App;
