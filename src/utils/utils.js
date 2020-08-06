c759219e413768c6694e5e4817ee3d28c9c87fce
buingoctai <buingoctai994@gmail.com>
2020-08-06 18:16:26 +0700

    fix for any direction moving

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,18 +5,18 @@ export const vailidateUrl = (url) => {
 }
 
 export const detectValues = (start, end) => {
-    console.log('start', start, ' end', end);
     const deltaX = end.x - start.x
     const deltaY = end.y - start.y
 
-    if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX > 0) {
-        return { top: DEFAULT_TOP, left: DEFAULT_LEFT + deltaX };
-    } else if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0) {
-        return { top: DEFAULT_TOP, left: DEFAULT_LEFT + deltaX };
-    } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
-        return { top: DEFAULT_TOP + deltaY, left: DEFAULT_LEFT };
-    } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
-        return { top: DEFAULT_TOP + deltaY, left: DEFAULT_LEFT };
-    }
-    return { top: DEFAULT_TOP, left: DEFAULT_LEFT }
+    // if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX > 0) {
+    //     return { top: DEFAULT_TOP, left: DEFAULT_LEFT + deltaX };
+    // } else if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0) {
+    //     return { top: DEFAULT_TOP, left: DEFAULT_LEFT + deltaX };
+    // } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
+    //     return { top: DEFAULT_TOP + deltaY, left: DEFAULT_LEFT };
+    // } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
+    //     return { top: DEFAULT_TOP + deltaY, left: DEFAULT_LEFT };
+    // }
+
+    return { top: DEFAULT_TOP + deltaY, left: DEFAULT_LEFT + deltaX }
 }
